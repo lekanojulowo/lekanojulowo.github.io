7cde941ede3344376dc818a25ba37622831ba862
Lekan Ojulowo <lekanojulowo@gmail.com>
2020-04-11 18:15:01 +0100

    add css functionalities

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// constant declaration for querySelector(All)
+const $q = document.querySelector.bind(document);
+const $qa = document.querySelectorAll.bind(document);
+const loadImage = (id, targetId) => {
+  let el = $q(`#${id}`);
+  let targetEl = targetId ? $q(`#${targetId}`) : el;
+  let imageToLoad;
+
+  if (el.dataset.image)
+    imageToLoad = el.dataset.image;
+  else if (typeof el.currentSrc === 'undefined')
+    imageToLoad = el.src;
+  else imageToLoad = el.currentSrc;
+
+  if (imageToLoad) {
+    let img = new Image();
+    img.src = imageToLoad;
+    img.onload = function () {
+      targetEl.classList.add('is-loaded');
+    }
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  $q('#subscribe').focus();
+  loadImage('wallpaper');
+  loadImage('pictureImage', 'picture');
+});
